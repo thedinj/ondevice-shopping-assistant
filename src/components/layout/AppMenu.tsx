96030e823e8c849115ff1020ecf29de22a86ac8f
thedinj <thedinj@gmail.com>
2026-01-03 00:52:43 -0600

    Add icon to menu header

--- src/components/layout/AppMenu.tsx
+++ src/components/layout/AppMenu.tsx
@@ -1,11 +1,14 @@
 import {
     IonContent,
+    IonHeader,
     IonIcon,
     IonItem,
     IonLabel,
     IonList,
     IonMenu,
     IonMenuToggle,
+    IonTitle,
+    IonToolbar,
 } from "@ionic/react";
 import { settings } from "ionicons/icons";
 import { useAppHeader } from "./useAppHeader";
@@ -15,6 +18,31 @@ export const AppMenu: React.FC = () => {
 
     return (
         <IonMenu contentId="main-content" type="overlay">
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "12px",
+                            }}
+                        >
+                            <img
+                                src="/img/icon.png"
+                                alt="Basket Bot"
+                                style={{
+                                    width: "32px",
+                                    height: "32px",
+                                    borderRadius: "50%",
+                                    objectFit: "cover",
+                                }}
+                            />
+                            <span>Basket Bot</span>
+                        </div>
+                    </IonTitle>
+                </IonToolbar>
+            </IonHeader>
             <IonContent>
                 <IonList>
                     {/* Custom menu items from pages */}
